5c2f567c1bdd799f39deac35144ebf82cb05c210
Theilor <theilor@gmail.com>
2017-09-08 15:54:09 -0300

    mensagens de exceção

--- sum/src/main/java/s2b/tst/sum/Sum.java
+++ sum/src/main/java/s2b/tst/sum/Sum.java
@@ -4,9 +4,9 @@ public class Sum {
 
 	public static double sum(double d, double e) {
 		if(d<0)
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("o primeiro numero não pode ser negativo");
 		if(e<0)
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("o segundo numero não pode ser negativo");
 		
 		return d+e;
 	}
