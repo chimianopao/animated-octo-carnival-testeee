232c2b555abe6be30f807348ab10c4db616be5f0
Theilor <theilor@gmail.com>
2017-09-12 14:31:35 -0300

    Agora o usuario digita os valores para a  soma.

--- sum/src/main/java/s2btstsum/App.java
+++ sum/src/main/java/s2btstsum/App.java
@@ -1,19 +1,27 @@
 package s2btstsum;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        double a = 10;
-        double b = 20;
-        double r = Sum.sum(a, b);
-        System.out.println("olar");
-        //int a = 10;
         
+    	Scanner s = new Scanner(System.in);
+    	
+    	System.out.println("olar, digita um numero");
+        int ab = s.nextInt();
+        System.out.println("olar, digita outro");
+        int ac = s.nextInt();
+        //s.close();
+        double r = Sum.sum(ab, ac);
+        
+        System.out.println("soma = "+r);
         
     }
 }
